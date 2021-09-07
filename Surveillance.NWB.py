66eb9045592e0ced612e4c7159d3f7a17bd8b8d2
Muneeswari_Sundaraselvam <66647070+MuneeswariS1991@users.noreply.github.com>
2021-09-07 10:38:13 +0530

    Update Surveillance.NWB.py

--- Surveillance.NWB.py
+++ Surveillance.NWB.py
@@ -147,7 +147,7 @@ sample_text_fps = Label(window, bg='grey64', text="FPS: ",
 sample_text_fps.place(x=40, y=85)
 
 text_fps = Label(window, bg='grey64', textvariable=displayVar,
-                 font=("Times New Roman", 12, 'bold'))
+                 font=("Times New Roman", 12))
 text_fps.place(x=75, y=85)
 
 displayVarFAT = StringVar()
@@ -156,7 +156,7 @@ sample_text_fat = Label(window, bg='grey64', text="FAT: ",
 sample_text_fat.place(x=40, y=115)
 
 text_fat = Label(window, bg='grey64', textvariable=displayVarFAT,
-                 font=("Times New Roman", 12, 'bold'))
+                 font=("Times New Roman", 12))
 text_fat.place(x=75, y=115)
 
 displayVarPath = StringVar()
@@ -165,8 +165,8 @@ sample_text_path = Label(window, bg='grey64', text="Path To Output: ",
 sample_text_path.place(x=40, y=145)
 
 text_path = Label(window, bg='grey64', textvariable=displayVarPath,
-                  font=("Times New Roman", 12, 'bold'))
-text_path.place(x=140, y=145)
+                  font=("Times New Roman", 12))
+text_path.place(x=160, y=145)
 
 displayVar.set(str(0))
 displayVarFAT.set(str(0))
@@ -186,24 +186,19 @@ def open_popup():
     top.resizable(0, 0)
     top.title("Choose Video Source")
     var = IntVar()
-    or_label = Label(top, text="OR", font=(
-        "Times New Roman", 13, 'bold')).place(x=20, y=125)
-    R1 = Radiobutton(top, text="Browse Files",font=(
-        "Times New Roman", 12), variable=var,
+    or_label = Label(top, text="OR", font=("Times New Roman", 12, 'bold')).place(x=20, y=125)
+    R1 = Radiobutton(top, text="Browse Files", font=("Times New Roman", 12, 'bold'),variable=var,
                      value=1, command=switch_flag_for_browse).place(x=10, y=160)
-    username = Label(top, text="Username",font=(
-        "Times New Roman", 12)).place(x=10, y=10)
-    password = Label(top, text="Passowrd",font=(
-        "Times New Roman", 12)).place(x=10, y=50)
-    ip_address = Label(top, text="IP Address",font=(
-        "Times New Roman", 12)).place(x=10, y=90)
+    username = Label(top, text="Username",font=("Times New Roman", 12)).place(x=10, y=10)
+    password = Label(top, text="Password",font=("Times New Roman", 12)).place(x=10, y=50)
+    ip_address = Label(top, text="IP Address",font=("Times New Roman", 12)).place(x=10, y=90)
 
     E1 = Entry(top, bd=2, textvariable=un_entry).place(x=100, y=10)
     E2 = Entry(top, bd=2, textvariable=passwd_entry).place(x=100, y=50)
     E3 = Entry(top, bd=2, textvariable=ip_add_entry).place(x=100, y=90)
 
-    B1=Button(top, text="Submit", font=(
-        "Times New Roman", 13, 'bold'),command=submit).place(x=250, y=190)
+    B1=Button(top, text="Submit", font=("Times New Roman", 12,'bold'),command=submit)
+    B1.place(x=250, y=190)
 
 
 def switch_flag_for_browse():
@@ -222,12 +217,14 @@ def browseFiles():
 
 
 def submit():
+    global path
     un = un_entry.get()
     pw = passwd_entry.get()
     ip = ip_add_entry.get()
-    url = f'rtsp://{un}:{pw}@{ip}/Streaming/Channels/102'
+    url = f'rtsp://{un}:{pw}@{ip}:554/Streaming/Channels/102'
     # print(url)
     path = url
+    print(path)
     un_entry.set("")
     passwd_entry.set("")
     ip_add_entry.set("")
@@ -266,7 +263,7 @@ def write_video(frames_list, fps, type, detur=False):
                       for Frame in frames_list]
     displayVarPath.set(str(f'{out_path}/{type}.mp4'))
     out = cv2.VideoWriter(f'{out_path}/{type}.mp4', cv2.VideoWriter_fourcc(*'MP4V'), fps, (frames_list[0].shape[1],
-                                                                                       frames_list[0].shape[0]))
+                                                                                           frames_list[0].shape[0]))
     for i in range(len(frames_list)):
         if detur:
             out.write(Frames_BGR[i].astype(np.uint8))
@@ -276,97 +273,100 @@ def write_video(frames_list, fps, type, detur=False):
 
 
 def toggleCapture():
-    global capture
+    try:
+        global capture
+        global path
+        if flag_for_browse:
+            path = browseFiles()
 
-    if flag_for_browse:
-        path = browseFiles()
-
-    print("OBJ DETECT PATH: ", path)
-    capture = VideoCapture(path)
+        print("OBJ DETECT PATH: ", path)
+        capture = VideoCapture(path)
+    except:
+        print("Exception Found")
 
 
 def objdetect():
     global iterFPS
+    global object_frames
     isVideoCaptureOpen = True
     i = 0
-    if keyboard.is_pressed('q') and isVideoCaptureOpen == True:
-        try:
-            isVideoCaptureOpen = False
-            capture.release()
-            L1.config(image='')
-            L2.config(image='')
-            displayVar.set(str(0))
-            write_video(object_frames, 10, 'object_detect')
-            print("Capture released")
-            return
-        except:
-            print("Some error has occured")
-    start = time.time()
-    (ret_old, old_frame) = capture.read()
-    iterFPS += 1
-    gray_oldframe = cvtColor(old_frame, COLOR_BGR2GRAY)
-    if(is_blur):
-        gray_oldframe = GaussianBlur(gray_oldframe, kernel_gauss, 0)
-    oldBlurMatrix = np.float32(gray_oldframe)
-    accumulateWeighted(gray_oldframe, oldBlurMatrix, 0.003)
-    ret, frame = capture.read()
-    #frame = cv2.flip(frame, 1)
-    inpframe = ImageTk.PhotoImage(
-        Image.fromarray(cvtColor(frame, COLOR_BGR2RGB)))
-    gray_frame = cvtColor(frame, COLOR_BGR2GRAY)
-    if(is_blur):
-        newBlur_frame = GaussianBlur(gray_frame, kernel_gauss, 0)
-    else:
-        newBlur_frame = gray_frame
 
-    newBlurMatrix = np.float32(newBlur_frame)
-    minusMatrix = absdiff(newBlurMatrix, oldBlurMatrix)
-    ret, minus_frame = threshold(minusMatrix, 60, 255.0, THRESH_BINARY)
-    accumulateWeighted(newBlurMatrix, oldBlurMatrix, 0.02)
+    while True:
+        start = time.time()
+        (ret_old, old_frame) = capture.read()
+        iterFPS += 1
+        gray_oldframe = cvtColor(old_frame, COLOR_BGR2GRAY)
+        if(is_blur):
+            gray_oldframe = GaussianBlur(gray_oldframe, kernel_gauss, 0)
+        oldBlurMatrix = np.float32(gray_oldframe)
+        accumulateWeighted(gray_oldframe, oldBlurMatrix, 0.003)
+        while True:
+            ret, frame = capture.read()
+            gray_frame = cvtColor(frame, COLOR_BGR2GRAY)
+            if(is_blur):
+                newBlur_frame = GaussianBlur(gray_frame, kernel_gauss, 0)
+            else:
+                newBlur_frame = gray_frame
+            newBlurMatrix = np.float32(newBlur_frame)
+            minusMatrix = absdiff(newBlurMatrix, oldBlurMatrix)
+            ret, minus_frame = threshold(minusMatrix, 60, 255.0, THRESH_BINARY)
+            accumulateWeighted(newBlurMatrix, oldBlurMatrix, 0.02)
+            inp = ImageTk.PhotoImage(Image.fromarray(frame))
+            L1['image'] = inp
+            # imshow('Input', frame)
 
-    drawRectangle(inpframe, frame, minus_frame, start, iterFPS)
+            # drawRectangle(frame, minus_frame)
+            if(is_blur):
+                minus_frame = GaussianBlur(minus_frame, kernel_gauss, 0)
+            minus_Matrix = np.float32(minus_frame)
+            if(is_close):
+                for i in range(get_current_value1()):
+                    minus_Matrix = dilate(minus_Matrix, kernel_d)
 
+                for i in range(get_current_value2()):
+                    minus_Matrix = erode(minus_Matrix, kernel_e)
 
-def drawRectangle(inp_frame, frame, minus_frame, start_time, iterFPS):
-    global object_frames
-    if(is_blur):
-        minus_frame = GaussianBlur(minus_frame, kernel_gauss, 0)
-    minus_Matrix = np.float32(minus_frame)
-    if(is_close):
-        for i in range(get_current_value1()):
-            minus_Matrix = dilate(minus_Matrix, kernel_d)
+            minus_Matrix = np.clip(minus_Matrix, 0, 255)
+            minus_Matrix = np.array(minus_Matrix, np.uint8)
+            contours, hierarchy = findContours(
+                minus_Matrix.copy(), RETR_TREE, CHAIN_APPROX_SIMPLE)
+            for c in contours:
+                (x, y, w, h) = boundingRect(c)
+                rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if(is_draw_ct):
+                    drawContours(frame, contours, -1, (0, 255, 255), 1)
+            # imshow('Object_Detection', frame)
+            end = time.time()
+            object_frames.append(frame)
+            frame = ImageTk.PhotoImage(Image.fromarray(frame))
+            fps = iterFPS/(end-start)
+            # displayVar.set(str(int(np.random.randint(18, 20))))
+            displayVar.set(str(int(fps)))
+            L2['image'] = frame
+            window.update()
 
-        for i in range(get_current_value2()):
-            minus_Matrix = erode(minus_Matrix, kernel_e)
-
-    minus_Matrix = np.clip(minus_Matrix, 0, 255)
-    minus_Matrix = np.array(minus_Matrix, np.uint8)
-    contours, hierarchy = findContours(
-        minus_Matrix.copy(), RETR_TREE, CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        (x, y, w, h) = boundingRect(c)
-        rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        if(is_draw_ct):
-            drawContours(frame, contours, -1, (0, 255, 255), 2)
-    end = time.time()
-    object_frames.append(frame)
-    out_frame = ImageTk.PhotoImage(Image.fromarray(
-        cvtColor(frame, cv2.COLOR_BGR2RGB)))
-    L1.imgtk = inp_frame
-    L1.configure(image=inp_frame)
-    L2.imgtk = out_frame
-    L2.configure(image=out_frame)
-    fps = iterFPS/(end-start_time)
-    displayVar.set(str(int(np.random.randint(19, 21))))
-    # displayVar.set(str(int(fps)))
-    displayVarFAT.set(str(0))
-    L2.after(1, objdetect)
+            if keyboard.is_pressed('q') and isVideoCaptureOpen == True:
+                try:
+                    isVideoCaptureOpen = False
+                    capture.release()
+                    L1.config(image='')
+                    L2.config(image='')
+                    displayVar.set(str(0))
+                    write_video(object_frames, 10, 'object_detect')
+                    print("Capture released")
+                    return
+                except:
+                    print("Some error has occured")
 
 
 def deturbulence():
-    if flag_for_browse:
-        path = browseFiles()
-    print("deturbulenceT PATH: ", path)
+    try:
+        global path
+        if flag_for_browse:
+            path = browseFiles()
+        print("deturbulenceT PATH: ", path)
+    except:
+        print("Exception Found")
     global ROI_enhanced_arr
     dataType = np.float32
     N_FirstReference = 10
@@ -503,6 +503,7 @@ def deturbulence():
 
 
 def endeturbulence():
+    global path
     if flag_for_browse:
         path = browseFiles()
     print("deturbulenceT PATH: ", path)
@@ -522,7 +523,8 @@ def endeturbulence():
     readVideo = 1
     ReferenceInitializationOpt = 2
 
-    ImagesSequence = loadVideo(0)
+    ImagesSequence = loadVideo(
+        'rtsp://Guest:srmguest123@192.168.1.64:554/Streaming/Channels/102')
     ImagesSequence = np.array(ImagesSequence).astype(dataType)
     roi = selectROI(ImagesSequence[0], resize_factor=2)
     roi_plate_250 = (1092, 830, 564, 228)
@@ -630,10 +632,136 @@ def endeturbulence():
     cv2.destroyAllWindows()
 
 
+def endeturbulence_old_version():
+    source_file = browseFiles()
+    dataType = np.float32
+    N_FirstReference = 10
+    L = 11
+    patch_size = (L, L)
+    patch_half_size = (
+        int((patch_size[0] - 1) / 2), int((patch_size[1] - 1) / 2))
+    patches_shift = 1
+    registration_interval = (15, 15)
+    R = 0.08
+    m_lambda0 = 0.55 * 10 ** -6
+    m_aperture = 0.06
+    m_focal_length = 250 * 10 ** -3
+    fno = m_focal_length / m_aperture
+    readVideo = 1
+    ReferenceInitializationOpt = 2
+    video_path = source_file
+    ImagesSequence = loadVideo(video_path)
+    # ImagesSequence = loadVideo()
+    ImagesSequence = np.array(ImagesSequence).astype(dataType)
+    roi = selectROI(ImagesSequence[0], resize_factor=2)
+    roi_plate_250 = (1092, 830, 564, 228)
+    roi_test = (310, 279, 200, 128)
+    if readVideo:
+        ROI_coord = roi
+    else:
+        ROI_coord = roi_plate_250
+
+    ROI_coord = (ROI_coord[1], ROI_coord[0], patch_size[1] * int(ROI_coord[3] / patch_size[1]),
+                 patch_size[0] * int(ROI_coord[2] / patch_size[0]))  # now roi[0] - rows!
+
+    ROI_arr = []
+    ROI_enhanced_arr = []
+    enhancedFrames = []
+    if ReferenceInitializationOpt == 1:  # option 1: "Lucky" reference frame.
+        FusedPatch = MaxSharpnessFusedPatch([frame[ROI_coord[0]:ROI_coord[0] + ROI_coord[2], ROI_coord[1]:ROI_coord[1] + ROI_coord[3]]
+                                             for frame in ImagesSequence[:N_FirstReference]], patch_half_size)
+        ReferenceFrame = ImagesSequence[N_FirstReference]
+        ReferenceFrame[ROI_coord[0] + patch_half_size[0]:ROI_coord[0] + ROI_coord[2] - patch_half_size[0],
+                       ROI_coord[1] + patch_half_size[1]:ROI_coord[1] + ROI_coord[3] - patch_half_size[1]] = FusedPatch
+        startRegistrationFrame = N_FirstReference
+    # option 2: Mean of N_FirstReference frames.
+    elif ReferenceInitializationOpt == 2:
+        ReferenceFrame = np.mean(ImagesSequence[:N_FirstReference], axis=0)
+        startRegistrationFrame = N_FirstReference
+    elif ReferenceInitializationOpt == 3:  # option 3: first frame
+        ReferenceFrame = ImagesSequence[0]
+        startRegistrationFrame = 1
+    else:
+        assert Exception("only values 1, 2 or 3 are acceptable")
+
+        # showImage(ReferenceFrame.astype(np.uint8))
+    enhancedFrames.append(ReferenceFrame)
+    i = 0
+    for frame in ImagesSequence[startRegistrationFrame:]:
+        t = time.time()
+        enhancedFrame = np.copy(frame)
+        ROI = frame[ROI_coord[0]:ROI_coord[0] + ROI_coord[2],
+                    ROI_coord[1]:ROI_coord[1] + ROI_coord[3]]
+        ROI_arr.append(ROI*255.0/ROI.max())
+
+        # Image Registration via optical flow
+        no_rows_Cropped_Frame, no_cols_Cropped_Frame = \
+            (ROI_coord[2] + 2 * registration_interval[0],
+             ROI_coord[3] + 2 * registration_interval[1])
+
+        u, v = optical_flow_tvl1(
+            ReferenceFrame[ROI_coord[0] - registration_interval[0]:ROI_coord[0] + ROI_coord[2] + registration_interval[0],
+                           ROI_coord[1] - registration_interval[1]:ROI_coord[1] + ROI_coord[3] + registration_interval[1]],
+            enhancedFrame[ROI_coord[0] - registration_interval[0]:ROI_coord[0] + ROI_coord[2] + registration_interval[0],
+                          ROI_coord[1] - registration_interval[1]:ROI_coord[1] + ROI_coord[3] + registration_interval[1]],
+            attachment=10, tightness=0.3, num_warp=3, num_iter=5, tol=4e-4, prefilter=False)
+
+        row_coords, col_coords = np.meshgrid(np.arange(no_rows_Cropped_Frame), np.arange(no_cols_Cropped_Frame),
+                                             indexing='ij')
+
+        warp(enhancedFrame[ROI_coord[0] - registration_interval[0]:ROI_coord[0] + ROI_coord[2] + registration_interval[0],
+                           ROI_coord[1] - registration_interval[1]:ROI_coord[1] + ROI_coord[3] + registration_interval[1]],
+             np.array([row_coords + v, col_coords + u]), mode='nearest', preserve_range=True).astype(dataType)
+
+        # Iterative averaging ROI
+        ReferenceFrame[ROI_coord[0]:ROI_coord[0] + ROI_coord[2], ROI_coord[1]:ROI_coord[1] + ROI_coord[3]] = \
+            (1 - R) * ReferenceFrame[ROI_coord[0]:ROI_coord[0] + ROI_coord[2], ROI_coord[1]:ROI_coord[1] + ROI_coord[3]] + \
+            R * frame[ROI_coord[0]: ROI_coord[0] + ROI_coord[2],
+                      ROI_coord[1]:ROI_coord[1] + ROI_coord[3]]
+        ROI_registered = ReferenceFrame[ROI_coord[0]:ROI_coord[0] +
+                                        ROI_coord[2], ROI_coord[1]:ROI_coord[1] + ROI_coord[3]]
+
+        m_lambda0 = 0.55 * 10 ** -6
+        m_aperture_diameter = 0.055
+        m_focal_length = 250 * 10 ** -3
+        fno = m_focal_length / m_aperture_diameter
+        ROI_reg_norm = ROI_registered / 255
+        k = (2 * np.pi) / m_lambda0  # wavenumber of light in vacuum
+        Io = 1.0  # relative intensity
+        L = 250  # distance of screen from aperture
+        X = np.arange(-m_aperture_diameter/2, m_aperture_diameter /
+                      2, m_aperture_diameter/70)  # pupil coordinates
+        Y = X
+        XX, YY = np.meshgrid(X, Y)
+        AiryDisk = np.zeros(XX.shape)
+        q = np.sqrt((XX-np.mean(Y)) ** 2 + (YY-np.mean(Y)) ** 2)
+        beta = k * m_aperture_diameter * q / 2 / L
+        AiryDisk = Io * (2 * j1(beta) / beta) ** 2
+        AiryDisk_normalized = AiryDisk/AiryDisk.max()
+        deblurredROI_wiener = wiener(ROI_reg_norm, psf=AiryDisk, balance=7)
+        deblurredROI = deblurredROI_wiener
+        deblurredROI = deblurredROI / deblurredROI.max() * 255.0
+        enhancedFrame[ROI_coord[0]:ROI_coord[0] + ROI_coord[2],
+                      ROI_coord[1]:ROI_coord[1] + ROI_coord[3]] = np.abs(deblurredROI)
+        ROI_enhanced_arr.append(deblurredROI)
+        enhancedFrames.append(enhancedFrame)
+        print('Frame analysis time: ', time.time() - t)
+        cv2.imshow('Input', ROI_arr[i].astype(np.uint8))
+        cv2.imshow('Output', ROI_enhanced_arr[i].astype(np.uint8))
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+        i += 1
+    cv2.destroyAllWindows()
+
+
 def deturbWithObjDetec():
-    if flag_for_browse:
-        path = browseFiles()
-    print("deturbulenceT PATH: ", path)
+    try:
+        global path
+        if flag_for_browse:
+            path = browseFiles()
+        print("deturbulenceT PATH: ", path)
+    except:
+        print("Exception Found")
     dataType = np.float32
     N_FirstReference = 10
     L = 11
@@ -648,7 +776,6 @@ def deturbWithObjDetec():
     m_aperture = 0.06
     m_focal_length = 250 * 10 ** -3
     fno = m_focal_length / m_aperture
-
     cap = cv2.VideoCapture(path)
     ImagesSequenceList = []
     ROI_arr_to_save = []
@@ -660,6 +787,7 @@ def deturbWithObjDetec():
     # ImagesSequence = loadVideo(0)
     while True:
         ret, frame = cap.read()
+        print(ret)
         itr_fps += 1
         end = time.time()
         if keyboard.is_pressed('q'):
@@ -831,10 +959,10 @@ C3 = Button(window, text="Object Detection", font=(
 C4 = Button(window, text="Turbulence Mitigation", font=(
     "Times New Roman", 12, 'bold'), command=deturbulence).place(x=1090, y=10)
 C5 = Button(window, text="Enhanced - TM", font=("Times New Roman",
-                                                12, 'bold'), command=endeturbulence).place(x=1090, y=60)
+                                                12, 'bold'), command=endeturbulence_old_version).place(x=1090, y=60)
 C6 = Button(window, text="TM + Detection", font=("Times New Roman",
                                                  12, 'bold'), command=deturbWithObjDetec).place(x=1280, y=10)
-C7 = Button(window, text="Change Input", font=("Times New Roman",
+C7 = Button(window, text="Switch Input", font=("Times New Roman",
                                                12, 'bold'), command=open_popup).place(x=1280, y=60)
 
 
